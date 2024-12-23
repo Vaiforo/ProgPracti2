70a8a750f54bdd468ce5189d7120afe2b3ab3585
Andy <abobkof@mail.ru>
2024-12-23 09:00:53 +0600

    ...

--- 4fourth.py
+++ 4fourth.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sympy import log, sqrt
+import matplotlib.pyplot as plt
 
 """
 1) Используйте функцию и отрезок ([a, b]) из вашего варианта задания 2
@@ -26,3 +27,149 @@ print(f"Значение функции для всех точек X:\n{y_values
 """
 print("\n\t\tЗадание 2\n")
 
+_, axes1 = plt.subplots(figsize=(10, 6))
+axes1.plot(x_parts, y_values, label='y = log(sqrt(x))', color='r')
+
+axes1.set_title('График функции: y = log(sqrt(x))', fontsize=16)
+axes1.set_xlabel('x', fontsize=14)
+axes1.set_xlabel('y', fontsize=14)
+
+axes1.legend(fontsize=12)
+
+plt.show()
+
+"""
+3) Постройте точечный график функции, в качестве меток для точек
+используйте любой доступный маркер. Задайте графику название. Подпишите оси. Измените
+цвет точек на желаемый. Цвет задайте с помощью указания значений
+красного, зеленого и синего (по модели RGB). Добавьте на график сетку.
+Укажите цвет и непрозрачность линий сетки.
+"""
+print("\n\t\tЗадание 3\n")
+
+plt.figure(figsize=(10, 6))
+plt.scatter(x_parts, y_values, label='y = log(sqrt(x))', color=(0.9, 0, 0.1), marker='8')  # RGB color for points
+
+plt.title('График функции: y = log(sqrt(x))', fontsize=16)
+plt.xlabel('x', fontsize=14)
+plt.ylabel('y', fontsize=14)
+
+plt.legend(fontsize=12)
+
+plt.grid(color='gray', alpha=0.5)
+
+plt.show()
+
+"""
+4) Используйте выборки из равномерного и нормального распределения
+(из задания 3). Постройте две гистограммы. Используйте различные
+цвета для гистограмм. Установите для каждой гистограммы собственное
+значение количества разбиений. Подпишите гистограммы.
+"""
+print("\n\t\tЗадание 4\n")
+
+uniform_vector = np.random.uniform(0, 100, 100).astype(int)
+normal_vector = np.random.normal(0, 10, 100).astype(int)
+
+plt.figure(figsize=(14, 6))
+
+plt.subplot(1, 2, 1)
+plt.hist(uniform_vector, bins=20, color=(0.1, 0.5, 0.8), edgecolor='black')
+plt.title('Histogram of Uniform Distribution')
+plt.xlabel('Value')
+plt.ylabel('Frequency')
+
+plt.subplot(1, 2, 2)
+plt.hist(normal_vector, bins=15, color=(0.8, 0.3, 0.1), edgecolor='black')
+plt.title('Histogram of Normal Distribution')
+plt.xlabel('Value')
+plt.ylabel('Frequency')
+
+plt.tight_layout()
+plt.show()
+
+"""
+5) Сгенерируйте выборку из равномерного распределения, состоящую из
+50 целых чисел от 1 до 5 (не включая 5). Посчитайте, сколько раз
+встречается каждое из чисел в выборке (без циклов!). На основе
+полученной информации постройте круговую и столбчатую диаграммы.
+Подпишите диаграммы. Для круговой диаграммы укажите подписи
+секторов. Для столбчатой диаграммы укажите подписи столбцов. Для
+обеих диаграмм укажите множество используемых цветов (для
+раскраски секторов и столбцов).
+"""
+print("\n\t\tЗадание 5\n")
+
+uniform_vector = np.random.uniform(1, 5, 50).astype(int)
+values, counters = np.unique(uniform_vector, return_counts=True)
+
+colors = [(0.93, 0.32, 0.32), (0.36, 0.73, 0.32), (0.32, 0.44, 0.73), (1.00, 0.98, 0.32)]
+
+plt.figure(figsize=(14, 6))
+
+plt.subplot(1, 2, 1)
+plt.pie(counters, labels=values, colors=colors, autopct='%d%%')
+plt.title('Pie chart of number distribution')
+
+plt.subplot(1, 2, 2)
+plt.bar(values, counters, color=colors, edgecolor='black')
+plt.xticks(values)
+
+plt.xlabel('Numbers')
+plt.ylabel('Counters')
+plt.title('Column chart of number distribution')
+
+plt.tight_layout()
+plt.show()
+
+"""
+6) Используя знания о разбиении отрезка на множество точек (из пункта
+1), а также функцию с несколькими переменными из задания 2 (которая
+использовалась для решения задачи оптимизации), постройте
+трехмерный график функции. Установите цвет линии на графике.
+Важно: если в вашем варианте к заданию 2 функция состояла более чем
+из двух переменных - используйте только первые две переменные.
+"""
+print("\n\t\tЗадание 6\n")
+
+
+def objective(x):
+    return (x[0] - 3) ** 2 + (x[1] - 1) ** 2
+
+
+a, b = 1, 6
+x_parts = np.linspace(a, b, 100)
+y_parts = np.linspace(a, b, 100)
+
+x, y = np.meshgrid(x_parts, y_parts)
+z = objective(np.array([x, y]))
+
+figure = plt.figure(figsize=(10, 6))
+axes = figure.add_subplot(111, projection='3d')
+
+figure.colorbar(axes.plot_surface(x, y, z, cmap='viridis', edgecolor='black'))
+
+axes.set_xlabel('x')
+axes.set_ylabel('y')
+axes.set_zlabel('Objective function values')
+axes.set_title('Plot of the objective function')
+
+plt.show()
+
+"""
+7) Используя подграфики (subplots) постройте сетку 2x2 из 4 графиков.
+Установите размер сетки. Добавьте заголовок сетки, укажите размер
+шрифта. В левом верхнем углу сетки разместите график из пункта 2
+текущего задания, в правом верхнем - из пункта 3. В левом нижнем -
+круговую диаграмму из пункта 5. В правом нижнем - трехмерный
+график из пункта 6. Для любого из графиков установите цвет фона.
+"""
+print("\n\t\tЗадание 7\n")
+
+# figure, axes = plt.subplots(figsize=(10, 10))
+#
+# figure.suptitle('Сетка графиков 2х2', fontsize=20)
+# figure.add_subplot(2, 2, 1)
+# figure.add_axes(axes1)
+#
+# plt.show()
---
+++ 5fiveth.py
@@ -0,0 +1,106 @@
+import pandas as pd
+
+"""
+1) Создайте датафрейм с следующими данными:
+Name Age Sex
+Strom, Mrs. Wilhelm (Elna Matilda Persson) 29 female
+Navratil, Mr. Michel ("Louis M Hoffman") 36,5 male
+Minahan, Miss. Daisy E 33 female
+"""
+print("\n\t\tЗадание 1\n")
+
+data = {
+    'Name': [
+        'Strom, Mrs. Wilhelm (Elna Matilda Persson)',
+        'Navratil, Mr. Michel ("Louis M Hoffman")',
+        'Minahan, Miss. Daisy E'
+    ],
+    'Age': [29, 36.5, 33],
+    'Sex': ['female', 'male', 'female']
+}
+
+dataframe1 = pd.DataFrame(data)
+dataframe1.columns = dataframe1.columns.str.lower()
+print(dataframe1)
+
+"""
+2) Загрузите данные из файла:
+https://drive.google.com/file/d/16gikf9vvMLyldkdHtoDYrPDbqTHP1BxG/view?usp=share_link
+"""
+print("\n\t\tЗадание 2\n")
+
+dataframe2 = pd.read_csv('titanic_csv.csv', encoding='utf-8', sep=';')
+dataframe2.columns = dataframe2.columns.str.lower()
+print(dataframe2)
+
+"""
+3) Загрузите напрямую данные по ссылке: https://clck.ru/3FNueV (read_csv).
+"""
+print("\n\t\tЗадание 3\n")
+
+dataframe3 = pd.read_csv(
+    'https://gist.githubusercontent.com/zaryanezrya/8b4ef51c707cb16d5e88a44dc00a1bb2/raw/41230f49c6268e072dbf102672f670be256922ab/gistfile1.txt',
+    encoding='utf-8', sep=',')
+print(dataframe3)
+
+"""
+4) Объедините датафреймы из п. 2, 3 в один датафрейм, удалите дубликаты строк.
+"""
+print("\n\t\tЗадание 4\n")
+
+dataframe = pd.concat([dataframe2, dataframe3], ignore_index=True)
+dataframe.drop_duplicates(inplace=True)
+print(dataframe)
+
+"""
+5) Задайте в качестве индекса датафрейма id пассажира.
+Отсортируйте по индексу (sort_values).
+"""
+print("\n\t\tЗадание 5\n")
+
+dataframe.set_index('passengerid', inplace=True)
+dataframe.sort_values(by='passengerid', inplace=True)
+print(dataframe)
+
+"""
+6) Отобразите информацию и базовую статистику по датафрейму (info, describe)
+"""
+print("\n\t\tЗадание 6\n")
+
+print(dataframe.info())
+print(dataframe.describe())
+
+"""
+7) Поменяйте местами нулевую по порядку строку с элементом с индексом 2 (loc, iloc).
+"""
+print("\n\t\tЗадание 7\n")
+
+dataframe.iloc[0], dataframe.loc[2] = dataframe.loc[2].values, dataframe.iloc[0].values
+print(dataframe)
+
+"""
+8) В столбце Sex замените female/male на f/m (map).
+"""
+print("\n\t\tЗадание 8\n")
+
+dataframe['sex'] = dataframe['sex'].map({'female': 'f', 'male': 'm'})
+print(dataframe)
+
+"""
+9) Найдите билеты по которым плыло 6 и более человек, отобразите список людей. (groupby, count).
+"""
+print("\n\t\tЗадание 9\n")
+
+tickets_groups = dataframe.groupby('ticket').size().reset_index(name='сount')
+tickets_with_count_more_5 = tickets_groups[tickets_groups['сount'] >= 6]['ticket'].tolist()
+result = dataframe[dataframe['ticket'].isin(tickets_with_count_more_5)]
+sorted_result = result.sort_values(by='ticket')
+
+print("Список людей по билетам с количеством пассажиров 6 и более:")
+print(sorted_result[['name', 'ticket']])
+
+"""
+10) Найдите людей, которые плыли в одной каюте с людьми из датафрейма из п1.
+"""
+print("\n\t\tЗадание 10\n")
+
